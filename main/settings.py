406f4a432887f01326c322afe3cb341029a09ad4
Daniel <yanez.daniel0101@gmail.com>
2017-10-26 17:58:14 -0500

    initial

--- main/settings.py
+++ main/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'apps.login',
-    'apps.pokes',
+#    'apps.pokes',    CHANGE THIS !!!
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
